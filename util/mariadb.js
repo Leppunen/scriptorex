690a80af1eb51ddc594a715b3a623c8e62bcd6d7
Leppunen <niko@ivr.fi>
2019-11-28 21:20:35 +0200

    Read database credentials from enviroment

--- util/mariadb.js
+++ util/mariadb.js
@@ -1,6 +1,15 @@
 const mariadb = require('mariadb');
-const config = require('../config');
-const pool = mariadb.createPool(config.db);
+const options = {
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME,
+    waitForConnections: true,
+    connectionLimit: 25,
+    queueLimit: 0,
+    rowsAsArray: false,
+};
+const pool = mariadb.createPool(options);
 
 pool.getConnection((err) => {
     if (err) {
