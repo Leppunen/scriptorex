bb7de37daf999f64a8baf7c0ceeba3e19e930440
Leppunen <niko@ivr.fi>
2020-06-16 21:34:10 +0300

    Add custom code to keyword module and refactor

--- modules/keyword.js
+++ modules/keyword.js
@@ -1,12 +1,16 @@
-module.exports.get = async (meta, opts = {}) => {
-    const kwData = sc.Data.keywords.find((i) => i.Channel === meta.channelMeta.ID && new RegExp(`^${i.Name}$`, i.Case_Sensitive === 1 ? 'gu' : 'giu').test(meta.message.text));
+module.exports.check = (meta) => {
+    return Boolean(sc.Data.keywords.find((i) => i.Channel === meta.channelMeta.ID && new RegExp(i.Exact ? `^${i.Name}$` : i.Name, i.Case_Sensitive === 1 ? 'gu' : 'giu').test(meta.message.text)));
+};
+
+module.exports.get = async (meta) => {
+    const kwData = sc.Data.keywords.find((i) => i.Channel === meta.channelMeta.ID && new RegExp(i.Exact ? `^${i.Name}$` : i.Name, i.Case_Sensitive === 1 ? 'gu' : 'giu').test(meta.message.text));
     const user = await sc.Modules.user.get({Platform: meta.platform, id: meta.user.id, name: meta.user.login});
 
     if (!kwData) {
         return null;
     }
 
-    if (await sc.Utils.cache.get(`cooldown-kword-${meta.channelMeta.ID}-${kwData.Name}`)) {
+    if (await sc.Utils.cache.get(`cooldown-kword-${meta.channelMeta.ID}-${kwData.ID}`)) {
         return null;
     }
 
@@ -14,7 +18,26 @@ module.exports.get = async (meta, opts = {}) => {
         return null;
     }
 
-    let kwResp = kwData.Reply;
+    let kwResp;
+
+    switch (kwData.Type) {
+    case 'Text':
+        kwResp = kwData.Data;
+        break;
+    case 'Code':
+        try {
+            kwResp = await eval(kwData.Data)(meta);
+        } catch (e) {
+            sc.Logger.error(`Keyword ${kwData.ID} Failed -> ${e}`);
+            if (e.response) {
+                sc.Logger.json(e.response);
+            }
+            return `Keyword ${kwData.ID} Failed.`;
+        }
+        break;
+    default:
+        return null;
+    }
 
     // Check the keyword reply against banphrases
     if (!kwData.SkipBanphrases) {
@@ -22,10 +45,30 @@ module.exports.get = async (meta, opts = {}) => {
         kwResp = await sc.Modules.banphrase.pajbot(meta, kwResp);
     }
 
-    if (kwData.Cooldown && opts.setCooldown) {
-        await sc.Utils.cache.set(`cooldown-kword-${meta.channelMeta.ID}-${kwData.Name}`, 'true', kwData.Cooldown);
+    if (kwData.Cooldown) {
+        await sc.Utils.cache.set(`cooldown-kword-${meta.channelMeta.ID}-${kwData.ID}`, 'true', kwData.Cooldown);
     }
 
-    return kwResp;
+    await sc.Utils.misc.log(
+        'Keyword',
+        meta.platform,
+        meta.channelMeta.ID,
+        user.ID || null,
+        `${kwData.Name} ${meta.message.args.join(' ')}`,
+        JSON.stringify({
+            User: meta.user.name,
+            Login: meta.user.login,
+            Channel: meta.channel,
+            Description: meta.channelMeta.Description,
+        },
+        null, 2),
+        kwResp,
+    );
+
+    if (typeof kwResp === 'object') {
+        return kwResp;
+    } else {
+        return String(kwResp);
+    }
 };
 
